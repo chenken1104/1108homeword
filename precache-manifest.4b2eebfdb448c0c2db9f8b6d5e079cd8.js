dabca0503667511b8a62a71f73aca49706f17dff
1108 <a0976504007@gmail.com>
2020-11-08 21:26:53 +0800

    Updates

---
+++ precache-manifest.4b2eebfdb448c0c2db9f8b6d5e079cd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea78e125dfd3ed57d1e4b0ca24db597",
+    "url": "/1108homeword/index.html"
+  },
+  {
+    "revision": "81bb95f90ddd0c512dcd",
+    "url": "/1108homeword/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "851cb6c3782e9a6d6616",
+    "url": "/1108homeword/static/css/main.6c1ee0cd.chunk.css"
+  },
+  {
+    "revision": "81bb95f90ddd0c512dcd",
+    "url": "/1108homeword/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1108homeword/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851cb6c3782e9a6d6616",
+    "url": "/1108homeword/static/js/main.52c10aaf.chunk.js"
+  },
+  {
+    "revision": "f457a939e6f37bb71f0b",
+    "url": "/1108homeword/static/js/runtime-main.28c9b222.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/1108homeword/static/media/SEGA.82e09f78.TTF"
+  }
+]);
