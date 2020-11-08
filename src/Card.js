a78fe6ab7e71b9b416eaf51e575a15cad77e7dae
1108 <a0976504007@gmail.com>
2020-11-08 21:19:27 +0800

    Initial commit, saving classprojust files

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+ 
+const Card = ({  name, email,image }) => {
+    const styles = {
+        width: '200px',
+        height:'200px'
+    };
+    return (
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`${process.env.PUBLIC_URL}${image}`} style={styles} alt="robots" />            
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
+}
+ 
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,74 @@
+// robots.js
+
+export const robots = [
+    {
+        id: 1,
+        image:"/img/1.jpg",
+        name: '網頁計算機',
+        username: 'Bret',
+        email: '模擬計算機'
+    },
+    {
+        id: 2,
+        image:"/img/2.png",
+        name: '打怪小遊戲',
+        username: 'Antonette',
+        email: 'Monster Game'
+    },
+    {
+        id: 3,
+        image:"/img/3.png",
+        name: '記帳查詢工具',
+        username: 'Samantha',
+        email: ''
+    },
+    {
+        id: 4,
+        image:"/img/4.png",
+        name: '自訂長條圖',
+        username: 'Karianne',
+        email: '不規則長條圖'
+    },
+    {
+        id: 5,
+        image:"/img/5.png",
+        name: '模擬卡片',
+        username: 'Kamren',
+        email: 'Card'
+    },
+    {
+        id: 6,
+        image:"/img/6.png",
+        name: '美食查詢列表',
+        username: 'Leopoldo_Corkery',
+        email: 'Food'
+    },
+    {
+        id: 7,
+        image:"/img/7.png",
+        name: '卡片商店',
+        username: 'Elwyn.Skiles',
+        email: 'Store'
+    },
+    {
+        id: 8,
+        image:"/img/8.jpg",
+        name: '自訂表單',
+        username: 'Maxime_Nienow',
+        email: 'Solo'
+    },
+    {
+        id: 9,
+        image:"/img/9.jpg",
+        name: '影片',
+        username: 'Delphine',
+        email: 'Player'
+    },
+    {
+        id: 10,
+        image:"/img/10.jpg",
+        name: 'Music Player',
+        username: 'Moriah.Stanton',
+        email: '音樂'
+    }
+];
